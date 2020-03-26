1a98a345a681fba694c3a58542a81f57c9850e91
anuragpatle <anurag.ap6@gmail.com>
2020-03-26 17:00:13 +0530

    Install 'Yargs' npm package for agruments parsing.
    ====================
    PS D:\NodeJs_WorkSpace\nodejs-notes-app> node app.js --version
    1.1.0
    ==================================
    PS D:\NodeJs_WorkSpace\nodejs-notes-app> node app.js --help
    app.js [command]
    
    Commands:
      app.js add     Add a new note
      app.js remove  Remove a note
      app.js list    List your notes
      app.js read    Read a note
    
    Options:
      --help     Show help                                                 [boolean]
      --version  Show version number                                       [boolean]
    
    ====================

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var chalk = require("chalk");
-const getMessage = require('./notes')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-printWithWhiteBg = function (str) {
-  const greenMsg = chalk.white.inverse.bold(str)
-  console.log(greenMsg)
-}
 
-const command = process.argv[2]
+// Customizing yargs number
+// PS D:\NodeJs_WorkSpace\nodejs-notes-app> node app.js --version
+// 1.1.0
+yargs.version('1.1.0')
 
-if (command === 'add') {
-  
-  printWithWhiteBg('Adding Note!')
+//console.log(yargs.argv)
 
-} else if (command === 'remove') {
-  
-  printWithWhiteBg('Remove Note!')
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+      console.log('Adding a new note!')
+  }
+})
 
-} 
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+      console.log('Removing the note')
+  }
+})
 
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+      console.log('Listing out all notes')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+      console.log('Reading a note')
+  }
+})
+
+const args = yargs.argv
--- notes.js
+++ notes.js
@@ -1,5 +1,12 @@
+var chalk = require("chalk");
+
 getNotes = function () {
     return 'Your notes'
+}
+
+printWithWhiteBg = function (str) {
+    const greenMsg = chalk.white.inverse.bold(str)
+    console.log(greenMsg)
 }
 
 module.exports = getNotes

e7ef8809e572738816104e56b6701905af9eda9d
anuragpatle <anurag.ap6@gmail.com>
2020-03-28 20:06:35 +0530

    Error Handeling

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,9 +5,15 @@ const mapboxToken = 'pk.eyJ1IjoiYXBhdGxlIiwiYSI6ImNrOGJuMDA0ZzA3aHkzZnA5Z2o0ZXBh
 
 const url = 'https://api.darksky.net/forecast/' + darkskyToken + '/37.8267,-122.4233'
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-})
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')   
+//     }
+// })
 
 
 // Geocoding
@@ -15,8 +21,15 @@ request({ url: url, json: true }, (error, response) => {
 // We are searching latitude and longitude of 'Los Angles'
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + mapboxToken + '&limit=1'
 
+
 request({ url: geocodeURL, json: true }, (error, response) => {
-    const latitude = response.body.features[0].center[0]
-    const longitude = response.body.features[0].center[1]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log('Unable to connect to location services!')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[0]
+        const longitude = response.body.features[0].center[1]
+        console.log(latitude, longitude)
+    }
 })

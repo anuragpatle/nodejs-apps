645546067a60f33328ed012ad2d0360cb7fbee45
anuragpatle <anurag.ap6@gmail.com>
2020-03-29 15:44:13 +0530

    Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,20 @@
 const { geoCode, forecast } = require('./utils/geocode')
 
-// geoCode('Pune', (error, data) => {
-//     console.log('Error', error)
-//     console.log(data)
-// })
+geoCode('Pune', (error, data) => {
 
+    if (error) {
+        return console.log('Error', error)
+    }
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log('Error', error)
+        }
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+        console.log('Location: ' + data.placeName)
+        console.log('Data', forecastData)
+    })
+
 })
+
+
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -6,39 +6,12 @@ const mapboxToken = 'pk.eyJ1IjoiYXBhdGxlIiwiYSI6ImNrOGJuMDA0ZzA3aHkzZnA5Z2o0ZXBh
 
 const foreCastUrl = 'https://api.darksky.net/forecast/' + darkskyToken + '/37.8267,-122.4233'
 
-// request({ url: foreCastUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-//     }
-// })
-
-
 // Geocoding
 // Address -> Lat/Long -> Weather
 // We are searching latitude and longitude of 'Los Angles'
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + mapboxToken + '&limit=1'
-
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[0]
-//         const longitude = response.body.features[0].center[1]
-//         console.log(latitude, longitude)
-//     }
-// })
-
-
 const geoCode = (address, callback) => {
     geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + mapboxToken + '&limit=1'
-
+    //console.log('geocodeURL: ' + geocodeURL)
     request({ url: geocodeURL, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to the network!')
@@ -47,8 +20,8 @@ const geoCode = (address, callback) => {
         } else {
 
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
                 placeName: response.body.features[0].place_name
             })
         }

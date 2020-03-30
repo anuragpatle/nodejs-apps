b8b4a18544d11dd3644160b4cfe2f6469c8324c2
anuragpatle <anurag.ap6@gmail.com>
2020-03-30 17:25:46 +0530

    Advance Templating: Using partials.
    Like master pages for header and footer

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,23 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 app.use(express.static(publicDirectoryPath))
 
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
+hbs.registerPartials(partialPath)
 
 // internally index will get mapped to:  ../views/index.hbs
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather app',
-        name: 'Anurag Patle'
+        name: 'Anurag Patle for indexpage'
     })
 
 })
@@ -22,7 +25,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Anurag Patle'
+        name: 'Anurag Patle for about'
     })
 
 })
@@ -37,7 +40,9 @@ app.get('/weather', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Some help para'
+        helpText: 'Some help para',
+        title: 'Help',
+        name: 'Anurag Patle for help'
     })
 })
 
